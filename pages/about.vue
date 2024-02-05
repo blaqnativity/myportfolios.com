36b3e7b49a43f73947b299085cb9ecb0d4882a2a
theolukayodeasemudara <blaqnativity@gmail.com>
2024-02-05 11:35:07 +0100

    page added

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="container mx-auto p-6 mt-10">
-    <div class="flex flex-col space-y-4 items-center text-center">
+  <div class="container mx-auto p-6 mt-4">
+    <!-- about text head -->
+    <div
+      class="flex flex-col space-y-4 items-center text-center justify-center"
+    >
       <h2 class="text-5xl font-medium text-gray-500 md:text-7xl">about.</h2>
       <span class="font-medium text-gray-500"
         >I'm a frontend engineer based in Lagos, Nigeria.</span
@@ -27,15 +30,66 @@
         adaptation. I am now eager to contribute my unique perspective and
         skills to the ever-evolving tech landscape.
       </p>
+
+      <div class="mt-14">
+        <button class="btn">Download CV</button>
+      </div>
+    </div>
+
+    <!-- services I offer -->
+    <div class="flex flex-col space-y-4 items-center justify-center mt-20">
+      <p class="text-gray-500 font-medium">Services I offer to my clients</p>
+      <h3 class="text-3xl font-medium">My Services</h3>
+    </div>
+
+    <!-- gird columns -->
+    <div class="grid mt-20 gap-8 md:grid-cols-3">
+      <!-- flex items -->
+      <div class="card">
+        <img class="w-[30%]" src="~/assets/img/html.svg" alt="" />
+        <h3>Front-end Development</h3>
+        <p>
+          I create responsive websites that allow the user to experience your
+          website in the best and most appropriate way suited to the device they
+          are using.
+        </p>
+      </div>
+      <div class="card">
+        <img class="w-[30%]" src="~/assets/img/web.svg" alt="" />
+        <h3>Web Design</h3>
+        <p>
+          I have excellent understanding of graphic design, colour schemes,
+          button design, interface design, Photoshop /Illustrator, navigational
+          design.
+        </p>
+      </div>
+      <div class="card">
+        <img class="w-[30%]" src="~/assets/img/figma.svg" alt="" />
+        <h3>UI/UX Design</h3>
+        <p>
+          I create user-friendly interfaces that enable users to understand how
+          to use complex technical products.
+        </p>
+      </div>
+      <div class="card">
+        <img class="w-[30%]" src="~/assets/img/wordpress.svg" alt="" />
+        <h3>Wordpress SIte Development</h3>
+        <p>
+          WordPress is the world’s most popular content management system which
+          I have many years of experience with.
+        </p>
+      </div>
+      <div class="card">
+        <img class="w-[30%]" src="~/assets/img/rocket.svg" alt="" />
+        <h3>Learn more</h3>
+        <p>
+          If you’d like to find out more about how I work and my process of
+          working with clients, please send me a message in the contact page and
+          give a little information about the project you have in mind.
+        </p>
+      </div>
     </div>
   </div>
-
-  <!-- =============footer section================ -->
-  <section>
-    <div class="container mx-auto grid items-center p-6 text-center">
-      <small>©2024 theolukayodeasemudara.com</small>
-    </div>
-  </section>
 </template>
 
 <script>
